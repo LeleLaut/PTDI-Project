c0fec46125f66a6f2173b0d8b465c2495441cdc1
Rizkysyam <rizkysyam19@gmail.com>
2023-07-23 23:09:48 +0700

    a

--- Ket/test.py
+++ Ket/test.py
@@ -9,7 +9,7 @@ fig = plt.figure()
 fig.suptitle('Grafik Ketinggian', fontsize=8, fontweight='bold')
 fig.set_size_inches(6, 4)
 fig2 = plt.figure()
-fig2.suptitle('Grafik pitch', fontsize=8, fontweight='bold')
+fig2.suptitle('Grafik Pitch, Roll, dan Yaw', fontsize=8, fontweight='bold')
 fig2.set_size_inches(6, 4)
 ax1 = fig.add_subplot(1, 1, 1)
 ax2 = fig2.add_subplot(1, 1, 1)
@@ -18,9 +18,9 @@ def generate_random_data(timestamp):
     with open('data.txt', 'a') as file:
         for i in range(1):
             ketinggian = random.randint(0, 100)
-            pitch = random.randint(-20, 20)
-            roll = random.randint(-20, 20)
-            yaw = random.randint(-20, 20)
+            pitch = random.randint(-5, 5)
+            roll = random.randint(-5, 5)
+            yaw = random.randint(-5, 5)
             file.write(f"{ketinggian}, {timestamp}, {pitch}, {roll}, {yaw}\n")
         print(ketinggian)
 
@@ -40,16 +40,26 @@ def animate(i):
             zs.append(float(z))
             vs.append(float(v))
             ws.append(float(w))
-    # print (xs + ys + zs)
+
+    # Limit the data to show only the last 5 points
+    xs = xs[-5:]
+    ys = ys[-5:]
+    zs = zs[-5:]
+    vs = vs[-5:]
+    ws = ws[-5:]
+
     ax1.clear()
     ax1.plot(xs, ys)
     ax1.set_ylabel("Ketinggian")
     ax1.set_xlabel("TimeStamp (s)")
+
     ax2.clear()
-    ax2.plot(xs, zs, xs, ws, xs, vs)
-    ax2.set_ylabel("pitch")
+    ax2.plot(xs, zs, label='Pitch')
+    ax2.plot(xs, ws, label='Roll')
+    ax2.plot(xs, vs, label='Yaw')
+    ax2.set_ylabel("Nilai")
     ax2.set_xlabel("TimeStamp (s)")
-    #plt.savefig("Hasil_Update_PNG.png")
+    ax2.legend()
 
 open('data.txt', 'w').close()
 
