e519df1f67dcb83b7280c4ff2e5fa6154634a485
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2023-07-25 09:17:28 +0700

    Update saving_data.ino

--- GAL/sd card module/saving_data/saving_data.ino
+++ GAL/sd card module/saving_data/saving_data.ino
@@ -1,40 +1,29 @@
 #include <SPI.h>
 #include <SD.h>
 
-const int chipSelect = D5; // Gunakan pin CS yang Anda hubungkan ke modul microSD
+const int chipSelect = 10; // Sesuaikan dengan pin Chip Select yang Anda gunakan
 
 void setup() {
   Serial.begin(9600);
-
-  // Inisialisasi koneksi SPI dan kartu microSD
+  
+  // Inisialisasi kartu microSD
   if (!SD.begin(chipSelect)) {
     Serial.println("Kartu microSD tidak terdeteksi!");
     return;
   }
   Serial.println("Kartu microSD terdeteksi!");
+
+  // Buat dan tulis data ke berkas data.txt
+  File dataFile = SD.open("data.txt", FILE_WRITE);
+  if (dataFile) {
+    dataFile.println("Ini adalah contoh data yang ditulis ke berkas.");
+    dataFile.close();
+    Serial.println("Berhasil menulis data ke berkas data.txt.");
+  } else {
+    Serial.println("Gagal membuat atau membuka berkas data.txt.");
+  }
 }
 
 void loop() {
-  // Contoh penulisan data ke kartu microSD
-  File dataFile = SD.open("data.txt", FILE_WRITE);
-  if (dataFile) {
-    dataFile.println("Ini adalah contoh data yang ditulis ke kartu microSD.");
-    dataFile.close();
-    Serial.println("Berhasil menulis data ke kartu microSD.");
-  } else {
-    Serial.println("Gagal menulis data ke kartu microSD.");
-  }
-
-  // Contoh membaca data dari kartu microSD
-  File dataFile = SD.open("data.txt");
-  if (dataFile) {
-    while (dataFile.available()) {
-      Serial.write(dataFile.read());
-    }
-    dataFile.close();
-  } else {
-    Serial.println("Gagal membaca data dari kartu microSD.");
-  }
-
-  delay(5000); // Tunggu 5 detik sebelum menulis/membaca ulang data
+  // Tidak ada yang perlu dilakukan pada loop
 }
