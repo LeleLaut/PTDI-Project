48e1035b84d8824d6ebb170a2bb43b3e2cf98fc9
kazarach <kazarachmain@gmail.com>
2023-08-10 09:54:49 +0700

    kkk

--- KAZ/SERVERLOCAL/thisArduPull.py
+++ KAZ/SERVERLOCAL/thisArduPull.py
@@ -7,6 +7,12 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 # Get the local IP address
 local_ip = s.getsockname()[0]
+
+ip_components = local_ip.split('.')
+# Change the last three digits to '255'
+modified_ip = '.'.join(ip_components[:-1] + ['255'])
+broadcast_address = modified_ip
+
 # Replace 'BROADCAST_IP' and 'PORT' with the appropriate values
 BROADCAST_IP = local_ip
 PORT = 51111
@@ -29,10 +35,7 @@ server_ip = local_ip
 server_port = 52222
 server_socket.bind((server_ip, server_port))
 
-ip_components = local_ip.split('.')
-# Change the last three digits to '255'
-modified_ip = '.'.join(ip_components[:-1] + ['255'])
-broadcast_address = modified_ip
+
 
 print(f"Listening for broadcasts from ESP8266 on port {PORT}")
 
@@ -55,7 +58,9 @@ while True:
             csv_writer.writerow(float_list)
             serialized_data = json.dumps(float_list)
             server_socket.sendto(serialized_data.encode('utf-8'), (broadcast_address, server_port))
-            print(f"Broadcasted: {serialized_data}")
+            # print(f"Broadcasted: {serialized_data}")
+            print(f"Received data from ESP8266: {decoded_data} from {esp8266_address[0]} on port {esp8266_address[1]}")
+
     
     except socket.timeout:
         # Timeout occurred, no data received
